9d122f03fd6cf187e2f2cb51c03def724d857eb9
Peng Dong <dongpeng101@gmail.com>
2021-02-22 21:24:04 -0500

    fixed lab1

--- lab/lab1.js
+++ lab/lab1.js
@@ -14,14 +14,16 @@ function draw() {
     c.fillStyle = "lightblue";
     c.fillRect(0, 0, canvas.width, canvas.height);
 
+    c.textAlign = 'center';
     for (let i = 0; i < 6; i++) {
         c.fillStyle = fruit[i].color;
         c.fillRect(i * 200, 500 - 20 * fruit[i].quantity, 200, fruit[i].quantity * 20);
         c.fill();
         c.fillStyle = "black";
         c.font = "30px Arial";
-        c.fillText(fruit[i].name, i * 200 + 55, 475);
-        c.fillText(fruit[i].quantity, i * 200 + 90, 450);
+
+        c.fillText(fruit[i].name, i * 200 + 100, 475);
+        c.fillText(fruit[i].quantity, i * 200 + 100, 450);
     }
 }
 document.addEventListener('DOMContentLoaded', draw);
