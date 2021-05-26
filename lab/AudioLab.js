5edb6167ff2a73887d0a2871e7d8488a269174ff
Peng Dong <dongpeng101@gmail.com>
2021-05-26 15:54:01 -0400

    adding audio lab

---
+++ lab/AudioLab.js
@@ -0,0 +1,24 @@
+function alarm() {
+    var x = document.getElementById("alarm");
+    x.play();
+}
+
+function growling() {
+    var x = document.getElementById("growling");
+    x.play();
+}
+
+function bird() {
+    var x = document.getElementById("bird");
+    x.play();
+}
+
+function dialtone() {
+    var x = document.getElementById("dialtone");
+    x.play();
+}
+
+function roar() {
+    var x = document.getElementById("roar");
+    x.play();
+}
